d254ace7465e330bb1cceb15190795fb113dfc4b
Dominik Völkel <voelkel.dominik@gmail.com>
2023-10-22 16:27:25 +0200

    split rendererprocess into cpp and hpp

--- wui/include/RenderProcessHandler.hpp
+++ wui/include/RenderProcessHandler.hpp
@@ -1,51 +1,37 @@
 #pragma once
+#include "include/wrapper/cef_helpers.h"
+#include "include/cef_render_process_handler.h"
 #include "include/wrapper/cef_message_router.h"
-#include "messageRouter/messageRouterConfig.hpp"
+
 namespace wui
 {
 
     // Implementation of CefApp for the renderer process.
     class RenderProcessHandler : public CefRenderProcessHandler
     {
-    public:
-        RenderProcessHandler() {}
-
-        // CefRenderProcessHandler methods:
-        void OnWebKitInitialized() override
-        {
-            // Create the renderer-side router for query handling.
-            message_router_ = CefMessageRouterRendererSide::Create(getMessageRouterConfig());
-        }
-
-        void OnContextCreated(CefRefPtr<CefBrowser> browser,
-                              CefRefPtr<CefFrame> frame,
-                              CefRefPtr<CefV8Context> context) override
-        {
-            message_router_->OnContextCreated(browser, frame, context);
-        }
-
-        void OnContextReleased(CefRefPtr<CefBrowser> browser,
-                               CefRefPtr<CefFrame> frame,
-                               CefRefPtr<CefV8Context> context) override
-        {
-            message_router_->OnContextReleased(browser, frame, context);
-        }
-
-        bool OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
-                                      CefRefPtr<CefFrame> frame,
-                                      CefProcessId source_process,
-                                      CefRefPtr<CefProcessMessage> message) override
-        {
-            return message_router_->OnProcessMessageReceived(browser, frame,
-                                                             source_process, message);
-        }
-
     private:
         // Handles the renderer side of query routing.
         CefRefPtr<CefMessageRouterRendererSide> message_router_;
 
         IMPLEMENT_REFCOUNTING(RenderProcessHandler);
-        DISALLOW_COPY_AND_ASSIGN(RenderProcessHandler);
-    };
+        RenderProcessHandler(const RenderProcessHandler &) = delete;
+        RenderProcessHandler &operator=(const RenderProcessHandler &) = delete;
 
+    public:
+        RenderProcessHandler() {}
+
+        // CefRenderProcessHandler methods:
+        void OnWebKitInitialized() override;
+
+        void OnContextCreated(CefRefPtr<CefBrowser> browser,
+                              CefRefPtr<CefFrame> frame,
+                              CefRefPtr<CefV8Context> context) override;
+        void OnContextReleased(CefRefPtr<CefBrowser> browser,
+                               CefRefPtr<CefFrame> frame,
+                               CefRefPtr<CefV8Context> context) override;
+        bool OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
+                                      CefRefPtr<CefFrame> frame,
+                                      CefProcessId source_process,
+                                      CefRefPtr<CefProcessMessage> message) override;
+    };
 }
---
+++ wui/src/RenderProcessHandler.cpp
@@ -0,0 +1,37 @@
+#include "RenderProcessHandler.hpp"
+#include "messageRouter/messageRouterConfig.hpp"
+
+namespace wui
+{
+
+    // CefRenderProcessHandler methods:
+    void RenderProcessHandler::OnWebKitInitialized()
+    {
+        // Create the renderer-side router for query handling.
+        message_router_ = CefMessageRouterRendererSide::Create(getMessageRouterConfig());
+    }
+
+    void RenderProcessHandler::OnContextCreated(CefRefPtr<CefBrowser> browser,
+                                                CefRefPtr<CefFrame> frame,
+                                                CefRefPtr<CefV8Context> context)
+    {
+        message_router_->OnContextCreated(browser, frame, context);
+    }
+
+    void RenderProcessHandler::OnContextReleased(CefRefPtr<CefBrowser> browser,
+                                                 CefRefPtr<CefFrame> frame,
+                                                 CefRefPtr<CefV8Context> context)
+    {
+        message_router_->OnContextReleased(browser, frame, context);
+    }
+
+    bool RenderProcessHandler::OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
+                                                        CefRefPtr<CefFrame> frame,
+                                                        CefProcessId source_process,
+                                                        CefRefPtr<CefProcessMessage> message)
+    {
+        return message_router_->OnProcessMessageReceived(browser, frame,
+                                                         source_process, message);
+    }
+
+}
